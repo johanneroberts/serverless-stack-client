1abc62f125da9098a0a414942f9595cd592c843a
Johannes Roberts <johannes.roberts@iress.com>
2020-07-13 17:35:46 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://ki1nq8zxgc.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_VubYho1G7",
+      APP_CLIENT_ID: "20apgenm5rtrert0v98pf17a34",
+      IDENTITY_POOL_ID: "eu-west-1:5a80232f-9aff-4570-aaa1-c95bbec42d7d"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Job cards</h1>
+        <p>A simple job card app</p>
+      </div>
+    </div>
+  );
+}
